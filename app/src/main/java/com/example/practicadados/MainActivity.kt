c5dcd1521b9a425cc8331ef5adde745a85f196fa
Liberto Baltasar Mendez <liberto.baltasar@gmail.com>
2024-10-24 19:02:44 +0200

    acabada

--- app/src/main/java/com/example/practicadados/MainActivity.kt
+++ app/src/main/java/com/example/practicadados/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.practicadados
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.practicadados.databinding.ActivityMainBinding
-import java.io.File
-import java.io.FileInputStream
 import kotlin.random.Random
 import kotlin.text.toIntOrNull
 
@@ -38,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         binding.casillero.visibility=View.INVISIBLE
         binding.pasar.visibility=View.INVISIBLE
         binding.Play.visibility = View.INVISIBLE
+        binding.indicadorEstado.visibility = View.INVISIBLE
+        binding.mensajeVictoria.visibility=View.INVISIBLE
+        binding.reset.visibility=View.INVISIBLE
         //TODO:implementar ocultar imagendado
     }
 
@@ -46,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         binding.j1Row.visibility = View.VISIBLE
         binding.j2Row.visibility = View.VISIBLE
         binding.Play.visibility = View.VISIBLE
+        binding.indicadorEstado.visibility = View.VISIBLE
         when(opcion){
             2->{
                 binding.j3Row.visibility=View.INVISIBLE
@@ -97,8 +94,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun gestionarTirada(numJugadores:Int){
         binding.Play.setOnClickListener {
-            var numero:Int
-            numero=tirarDado()
+            val numero:Int = tirarDado()
             seleccionarDado(numero)
             if (numero==1){
                 Toast.makeText(this@MainActivity,"Lo siento, turno del siguiente",Toast.LENGTH_SHORT).show()
@@ -115,18 +111,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pasarTurno(numJugadores: Int, fallo:Boolean){
+        val jugadorActual = binding.jugadorActual.text.toString().toInt()
+        val rondaActual = binding.rondaActual.text.toString().toInt()
         if (fallo){
             vaciarPuntuacionActual()
         }
         actualizarPuntuacionFinal()
+        //TODO:revisar el bucle
         binding.pasar.visibility=View.INVISIBLE
-        if (numJugadores>=binding.jugadorActual.text.toString().toInt()+1){
-            binding.jugadorActual.text=(binding.jugadorActual.text.toString().toInt()+1).toString()
+        if (rondaActual<5){
+            if (jugadorActual<numJugadores){
+                binding.jugadorActual.text =
+                    (jugadorActual + 1).toString()
+            }else{
+                binding.rondaActual.text =
+                    (rondaActual + 1).toString()
+                binding.jugadorActual.text = "1"
+            }
         }else{
-            binding.rondaActual.text=(binding.rondaActual.text.toString().toInt()+1).toString()
-            binding.jugadorActual.text="1"
-
+            if (jugadorActual<numJugadores){
+                binding.jugadorActual.text =
+                    (jugadorActual + 1).toString()
+            }else{
+                finalizarPartida()
+            }
         }
+    }
+
+    private fun finalizarPartida() {
+        Toast.makeText(this@MainActivity, "Has terminado la partida", Toast.LENGTH_SHORT).show()
+        binding.pasar.visibility=View.INVISIBLE
+        binding.Play.visibility = View.INVISIBLE
+        binding.indicadorEstado.visibility = View.INVISIBLE
+        binding.mensajeVictoria.text="Has ganado la partida jugador:${seleccionarGanador()}"
+        binding.mensajeVictoria.visibility=View.VISIBLE
+        binding.imagenDado.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dadovictoria))
+//        binding.reset.visibility=View.VISIBLE
+//        reiniciarPartida()
+    }
+
+    private fun seleccionarGanador():Int{
+        val puntuaciones = listOf(
+            binding.j1Total.text.toString().toIntOrNull() ?: 0,
+            binding.j2Total.text.toString().toIntOrNull() ?: 0,
+            binding.j3Total.text.toString().toIntOrNull() ?: 0,
+            binding.j4Total.text.toString().toIntOrNull() ?: 0
+        )
+
+        var ganador = 0
+        var maxPuntuacion = puntuaciones[0]
+
+        for (i in 1..3) {
+            if (puntuaciones[i] > maxPuntuacion) {
+                maxPuntuacion = puntuaciones[i]
+                ganador = i
+            }
+        }
+
+        return ganador + 1
     }
 
     private fun seleccionarDado(opcion: Int) {
@@ -156,6 +198,7 @@ class MainActivity : AppCompatActivity() {
         val suma = valor1 + valor2
         cellFinalView?.text = suma.toString()
     }
+
     private fun actualizarPuntuacionActualJugador(puntos: Int) {
         val row:Int=binding.jugadorActual.text.toString().toInt()
         val column:Int=binding.rondaActual.text.toString().toInt()
@@ -173,4 +216,10 @@ class MainActivity : AppCompatActivity() {
         val cellView = binding.casillero.findViewById<TextView>(cellId)
         cellView?.text = "0"
     }
+
+//    private fun reiniciarPartida(){
+//        binding.reset.setOnClickListener {
+//
+//    }
+
 }
